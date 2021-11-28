4247e18d83b3f3c96dba65af756b74d3d441363e
Klein Kristina <klein281293@gmail.com>
2021-11-28 12:36:48 +0700

    Init repo

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 633;
+        int moneyForMile = 20;
+
+        int bonus;
+        bonus = ticket / moneyForMile;
+        System.out.println(bonus);
+    }
+}
