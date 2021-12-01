311ed1093c750108fd9cf67a7c8df7bc42bd128e
Klein Kristina <klein281293@gmail.com>
2021-12-01 19:10:34 +0700

    Add class

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int ticket = 633;
-        int moneyForMile = 20;
-
-        int bonus;
-        bonus = ticket / moneyForMile;
-        System.out.println(bonus);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
