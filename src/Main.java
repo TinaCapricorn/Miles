a16f64d0a8a0314305bcecd679155141cbf1563d
Klein Kristina <klein281293@gmail.com>
2021-12-05 18:19:56 +0700

    Fix2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
-    public static void main(String[] args) {
-        BonusMilesService service = new BonusMilesService();
+    public static void main (String[] args) {
+        bonusMilesService service = new bonusMilesService();
         int price = 10_000;
         int miles = service.calculate(price);
         System.out.println(miles);
